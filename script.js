2f5d9dc1cfa9d463ebf1acae9d94dbef053efdc1
kkonorev <38575664+kkonorev@users.noreply.github.com>
2019-10-31 18:56:49 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+var app = new Vue({
+  el:"#main",
+  data:{
+  sodaList:[
+    {
+      name:"Doctor Pepper  ",
+      calories:"  75",
+      color:"blue"
+    },
+    {
+      name:"Sprite  ",
+      calories:"99",
+      color:"green"
+    },
+    {
+      name:"Big Red  ",
+      calories:"101",
+      color: "yellow"
+    }
+  ]//end listItems
+ }//end data
+});//end vue
